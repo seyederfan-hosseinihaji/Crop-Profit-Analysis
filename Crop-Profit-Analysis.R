f30f9eeeb6094d362757a497db3b20a01028721a
seyederfan-hosseinihaji <seyederfan.hosseinihaji@gmail.com>
2025-10-05 15:12:34 +0330

    feat: converted FAOSTAT.xls dataset into variables

---
+++ Crop-Profit-Analysis.R
@@ -0,0 +1,3 @@
+area <- c("Burundi", "China", "Germany", "Iran", "Italy", "USA")
+Yeild <- c(732.1, 5780.8, 7431.8, 2258.1, 3691.7, 3269.4) #kg/ha
+Area.Harvested <- c(10873, 23629200, 2897800, 6200000, 1867560, 15083610) #ha
